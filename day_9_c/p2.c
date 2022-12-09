c9910cbc4cab4114c1776457f45ef894c6f6401a
Ben Lirio <benjlirio@gmail.com>
2022-12-09 11:40:37 -0500

    nothing much

--- day_9_c/p2.c
+++ day_9_c/p2.c
@@ -4,23 +4,6 @@
 #define MAP_HEIGHT 10000
 #define MAP_WIDTH 10000
 #define ROPE_LEN 10
-struct pos {
-  int x;
-  int y;
-};
-
-unsigned char visited[MAP_HEIGHT][MAP_WIDTH];
-struct pos poses[ROPE_LEN];
-int unique_positions = 0;
-
-void update_visited() {
-  int x = poses[ROPE_LEN-1].x + MAP_WIDTH / 2;
-  int y = poses[ROPE_LEN-1].y + MAP_HEIGHT / 2;
-  if (!visited[x][y]) {
-    unique_positions++;
-    visited[x][y] = 1;
-  }
-}
 
 int abs(int x) {
   if (x < 0)
@@ -34,6 +17,26 @@ int max(int a, int b) {
   else
     return b;
 }
+
+struct pos {
+  int x;
+  int y;
+};
+
+unsigned char visited[MAP_HEIGHT][MAP_WIDTH];
+struct pos poses[ROPE_LEN];
+int unique_positions = 0;
+
+void update_visited() {
+  // Set origin to the center of the visited matrix
+  int x = poses[ROPE_LEN-1].x + MAP_WIDTH / 2;
+  int y = poses[ROPE_LEN-1].y + MAP_HEIGHT / 2;
+  if (!visited[x][y]) {
+    unique_positions++;
+    visited[x][y] = 1;
+  }
+}
+
 void follow() {
   for (int i = 1; i < ROPE_LEN; i++) {
     int dx = abs(poses[i-1].x - poses[i].x);
@@ -50,6 +53,7 @@ void follow() {
       poses[i].y = (poses[i-1].y + poses[i].y) / 2;
   }
 }
+
 void move(struct pos d_pos) {
   poses[0].x += d_pos.x;
   poses[0].y += d_pos.y;
@@ -57,35 +61,11 @@ void move(struct pos d_pos) {
   update_visited();
 }
 
-void show() {
-  for (int y = 4; y >= 0; y--) {
-    for (int x = 0; x < 6; x++) {
-      int printed = 0;
-      for (int i = 0; i < ROPE_LEN; i++) {
-        if (poses[i].x == x && poses[i].y == y) {
-          printed = 1;
-          printf("%d", i);
-          break;
-        }
-      }
-      if (printed)
-        continue;
-      else if (visited[y][x])
-        printf("#");
-      else
-        printf(".");
-    }
-    printf("\n");
-  }
-  getchar();
-}
-
 int main() {
   FILE* fp = fopen("input.txt", "r");
 
   char action;
   int value;
-  // show();
   for (;;) {
     int res = fscanf(fp, "%c %d\n", &action, &value);
     if (res == EOF)
@@ -103,7 +83,6 @@ int main() {
       if (action == 'D')
         d_pos.y = -1;
       move(d_pos);
-      // show();
     }
   }
   printf("unique_positions=%d\n", unique_positions);
